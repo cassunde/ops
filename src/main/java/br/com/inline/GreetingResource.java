c48b2182715b5b3cd41f44392ea1c8753a4eb7f3
Mattheus Cassundé <mattheus.089@gmail.com>
2023-02-23 14:59:59 -0300

    update controller

--- src/main/java/br/com/inline/GreetingResource.java
+++ src/main/java/br/com/inline/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Return hello() {
-        return new Return("test 6");
+        return new Return("test 7");
     }
 }
